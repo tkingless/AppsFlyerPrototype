f2f512f20ade26d6fe27659e5f11adbf83166441
tkingless <unstashworld@gmail.com>
2015-06-11 19:01:47 +0800

    add runtime script

---
+++ Assets/TestScene/AppsFlyerRuntime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppsFlyerRuntime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		AppsFlyer.setAppsFlyerKey ("L9sqRDPwTRURnhNNsyNagS"); 
+		AppsFlyer.setAppID ("com.nxtomo.flyer.test"); 
+		//AppsFlyer.setCollectIMEI (false);
+		//AppsFlyer.setCollectAndroidID (false); 
+		AppsFlyer.trackAppLaunch (); 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
