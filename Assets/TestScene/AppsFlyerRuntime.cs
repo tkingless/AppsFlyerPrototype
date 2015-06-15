08521cef78c2a68c13e2674a0eda0d112c8de363
tkingless <unstashworld@gmail.com>
2015-06-15 12:55:26 +0800

    update runtime

--- Assets/TestScene/AppsFlyerRuntime.cs
+++ Assets/TestScene/AppsFlyerRuntime.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AppsFlyerRuntime : MonoBehaviour {
 
@@ -11,6 +12,8 @@ public class AppsFlyerRuntime : MonoBehaviour {
 		//AppsFlyer.setCollectIMEI (false);
 		//AppsFlyer.setCollectAndroidID (false); 
 		AppsFlyer.trackAppLaunch (); 
+
+		print("AppsFlyerRuntime start() called");
 	
 	}
 	
@@ -18,4 +21,21 @@ public class AppsFlyerRuntime : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void OnGUI () {
+
+		if(GUI.Button(new Rect(Screen.width/2,Screen.height/2,Screen.width/8,Screen.height/8), "Shoot Test event")){
+			TrackRichEventTest();
+		}
+	}
+
+		// Test in app event
+		public void TrackRichEventTest() {
+			print ("trackRichEventTest called");
+			Dictionary<string, string> dict = new Dictionary<string, string> ();
+			dict.Add ("currency", "USD");
+			dict.Add ("productId", "123123");
+			dict.Add ("price", "100");
+			AppsFlyer.trackRichEvent ("add_to_wish_list", dict);
+		}
 }
