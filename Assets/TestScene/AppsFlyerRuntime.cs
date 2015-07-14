80a176978e658fa4145e37330813c0b8660c3bc4
tkingless <unstashworld@gmail.com>
2015-07-14 12:45:57 +0800

    add the second event in runtime button

--- Assets/TestScene/AppsFlyerRuntime.cs
+++ Assets/TestScene/AppsFlyerRuntime.cs
@@ -27,6 +27,9 @@ public class AppsFlyerRuntime : MonoBehaviour {
 		if(GUI.Button(new Rect(Screen.width/2,Screen.height/2,Screen.width/8,Screen.height/8), "Shoot Test event")){
 			TrackRichEventTest();
 		}
+		if(GUI.Button(new Rect(Screen.width/2+Screen.width/8,Screen.height/2+Screen.height/8,Screen.width/8,Screen.height/8), "Shoot Test event 2")){
+			TrackRichEventTest2();
+		}
 	}
 
 		// Test in app event
@@ -38,4 +41,13 @@ public class AppsFlyerRuntime : MonoBehaviour {
 			dict.Add ("price", "100");
 			AppsFlyer.trackRichEvent ("add_to_wish_list", dict);
 		}
+
+	public void TrackRichEventTest2() {
+		print ("trackRichEventTest2 called");
+		Dictionary<string, string> dict = new Dictionary<string, string> ();
+		dict.Add ("name", "Regal");
+		//dict.Add ("productId", "123123");
+		dict.Add ("price", "100");
+		AppsFlyer.trackRichEvent ("hotel_booked", dict);
+	}
 }
